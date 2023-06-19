5f07b003b38487fe92aa75342d332b638aa51cc9
NFadil <75855353+NFadil@users.noreply.github.com>
2023-06-19 09:20:56 +0700

    memperbaharui login

--- application/views/Login.php
+++ application/views/Login.php
@@ -1,86 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Login</title>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        .login-page {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-            background: linear-gradient(135deg, #c850c0, #4158d0);
-            background-size: 400% 400%;
-            animation: gradientBg 10s ease infinite;
-        }
-
-        @keyframes gradientBg {
-            0% {
-                background-position: 0% 50%;
-            }
-            50% {
-                background-position: 100% 50%;
-            }
-            100% {
-                background-position: 0% 50%;
-            }
-        }
-
-        .card {
-            width: 300px;
-            background-color: #fff;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-
-        .card-header {
-            
-            color: #000;
-            font-weight: bold;
-        }
-
-        .card-body {
-            padding: 20px;
-        }
-
-        .form-control {
-            margin-bottom: 10px;
-        }
-
-        .btn-login {
-            width: 100%;
-        }
-
-        .card-footer {
-            background-color: #fff;
-            border-top: 1px solid #eee;
-            padding: 10px;
-            text-align: center;
-        }
-    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
 </head>
-<body class="login-page">
-    <div class="card">
-        <div class="card-header text-center">Login</div>
-        <div class="card-body">
-            <form action="<?php echo site_url('Home/login');?>" method="post">
-                <label for="username" class="label">Username</label>
-                <input type="text" class="form-control" name="username" required>
-                <label for="password" class="label">Password</label>
-                <input type="password" class="form-control" name="password" required>
-                <br>
+<body>
+
+    <!----------------------- Main Container -------------------------->
+
+     <div class="container d-flex justify-content-center align-items-center min-vh-100">
+
+    <!----------------------- Login Container -------------------------->
+
+       <div class="row border rounded-5 p-3 bg-white shadow box-area">
+
+    <!--------------------------- Left Box ----------------------------->
+
+       <div class="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" style="background: #103cbe;">
+           <div class="featured-image mb-3">
+            <img src="http://localhost/RepositorySkripsi/Asset/img/1.png" class="img-fluid" style="width: 250px;">
+           </div>
+           <p class="text-white fs-2" style="font-family: 'Courier New', Courier, monospace; font-weight: 600;">Masuk</p>
+           <small class="text-white text-wrap text-center" style="width: 17rem;font-family: 'Courier New', Courier, monospace;">Untuk melihat skiripsi.</small>
+       </div> 
+
+    <!-------------------- ------ Right Box ---------------------------->
+        
+       <div class="col-md-6 right-box">
+          <div class="row align-items-center">
+                <div class="header-text mb-4">
+                     <h2>Hello,Again</h2>
+                     
+                </div>
+                <form action="<?php echo site_url('Home/login');?>" method="post">
+                <div class="input-group mb-3">
+                    <input name="username" type="text" class="form-control form-control-lg bg-light fs-6" placeholder="Email address">
+                </div>
+                <div class="input-group mb-1">
+                    <input name="password" type="password" class="form-control form-control-lg bg-light fs-6" placeholder="Password">
+                </div>
                 <?php echo $this->session->userdata("error");?>
-                <br>
-                <input type="submit" class="btn btn-primary btn-login" value="Login">
-        </div>
-        </form>
-        <div class="card-footer">
-            Tidak Punya Akun?
-            <br><a href="<?php echo site_url('Home/register') ?>">Register</a>
-        </div>
+                <div class="input-group mb-5 d-flex justify-content-between">
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="formCheck">
+                        <label for="formCheck" class="form-check-label text-secondary"><small>Remember Me</small></label>
+                    </div>
+                    <div class="forgot">
+                        <small><a href="#">Forgot Password?</a></small>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <button class="btn btn-lg btn-primary w-100 fs-6">Login</button>
+                </div>
+                </form>
+                
+          </div>
+       </div> 
+
+      </div>
     </div>
+
 </body>
 </html>
