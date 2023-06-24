93be54a8fbbd2286373a862b69b06a5e72cdf3ce
kukuhyulionsetiaprakoso <kukuhyulionsetiaprakoso@gmail.com>
2023-06-24 20:32:15 +0700

    Update Admin.php

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -2,7 +2,7 @@
 class Admin extends CI_Controller {
     function index(){
         $this->load->view("DashboardAdmin");
-        $this->load->view('Jurnal');
+        $this->load->view("Jurnal");
     }
 }
 ?>
