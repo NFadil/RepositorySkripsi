0f26d5511c712b1674bb4d727312d41e43870a36
kukuhyulionsetiaprakoso <kukuhyulionsetiaprakoso@gmail.com>
2023-06-25 21:34:59 +0700

    Update DashboardUser.php

--- application/views/DashboardUser.php
+++ application/views/DashboardUser.php
@@ -8,30 +8,14 @@
 					</div>
 					<div class="pull-left info">
 						<p><?php echo $this->session->userdata('username'); ?></p>
-<<<<<<< HEAD
-						<a href="#"><i class="fa fa-circle text-success"></i> <?php echo $this->session->userdata('level'); ?></a>
-					</div>
-				</div>
-				<form action="#" method="get" class="sidebar-form">
-					<div class="input-group">
-						<!-- <input type="text" name="q" class="form-control" placeholder="Search...">
-						<span class="input-group-btn">
-							<button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i></button>
-						</span> -->
-					</div>
-				</form>
-=======
 						<a href="#"><i class="fa fa-circle text-success"></i> Online</a>
 					</div>
 				</div>
->>>>>>> a02135d88525dc8cede2a0cac141b8a447649769
 				<!-- sidebar menu -->
 				<ul class="sidebar-menu" data-widget="tree">
 					<li class="header">MAIN NAVIGATION</li>
 					<li>
 						<a href="<?php echo site_url('Home');?>"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a>
-<<<<<<< HEAD
-=======
 					</li>
 					<li>
 						<a href=""><i class="fa fa-archive"></i> <span>Data Jurnal</span></a>
@@ -51,7 +35,6 @@
 							<li><a href="#"><i class="fa fa-circle-o"></i> Units</a></li>
 							<li><a href="#"><i class="fa fa-circle-o"></i> Items</a></li>
 						</ul>
->>>>>>> a02135d88525dc8cede2a0cac141b8a447649769
 					</li>
 					<li class="treeview">
 					<a href="#">
@@ -75,15 +58,13 @@
 				
 					<li class="header">SETTINGS</li>
 					<li><a href="<?php echo site_url('TambahUser'); ?>"><i class="fa fa-user"></i> <span>Tamabah User</span></a></li>
+					-->
 				</ul>
 			</section>
-<<<<<<< HEAD
 		</aside>
-=======
-		</aside>
+
 		
 		<!-- Content Wrapper -->
 		<div class="content-wrapper">
 			<!-- <?php echo $contents ?> -->
 		</div>
->>>>>>> a02135d88525dc8cede2a0cac141b8a447649769
