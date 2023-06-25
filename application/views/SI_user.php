5a8dd25ee88a27d4a6aaeebbb2a7b173d52f36be
kukuhyulionsetiaprakoso <kukuhyulionsetiaprakoso@gmail.com>
2023-06-25 23:43:19 +0700

    update

--- application/views/SI_user.php
+++ application/views/SI_user.php
@@ -18,15 +18,15 @@
       foreach ($sisteminformasi->result() as $row) {
       ?>
       <div class="col-xs-12">
-        <div class="box">
+        <div class="box" style=" padding: 0px 0px 100px 0px;">
           <div class="box-header"></div>
           <div class="box-body">
-            <div class="product">
-            <h1><?php echo $row->judul;?></h1>
+            <div class="product" style=" padding: 0px 0px 75px 100px;">
+            <h1 class="text-center" style=" padding: 0px 0px 50px 0px;"><?php echo $row->judul;?></h1>
               <div class="product-actions">
                 <iframe src="<?php echo $row->url;?>" width="50%" height="300px" style="float: left;"></iframe>
                 <br>
-                <div  class="attachment-text">
+                <div  class="attachment-text text-left" style="float: left; padding: 50px 0px 0px 200px;">
                   <h4>Nama: <?php echo $row->nama;?></h4>
                   <p>Jurusan: <?php echo $row->jurusan;?></p>
                   <p>Angkatan: <?php echo $row->angkatan;?></p>
