c3732f22f43ca5bf5cceb5be5307d93e47de3dea
kukuhyulionsetiaprakoso <kukuhyulionsetiaprakoso@gmail.com>
2023-06-26 00:00:14 +0700

    update about us

--- application/controllers/Aboutus.php
+++ application/controllers/Aboutus.php
@@ -16,6 +16,21 @@ class Aboutus extends CI_Controller{
         }
         
     }
+    function Kukuh(){
+        if($this->session->userdata('login')){
+            $level = $this->session->userdata('level');
+            $foto = $this->session->userdata('foto');
+            $this->load->view("NavbarUser");
+            $this->load->view("DashboardUser");
+            $this->load->view('Kukuh');
+            $this->load->view("Footer");
+           
+    
+        }else{
+            $this->load->view("login");
+        }
+        
+    }
         
         
         
