a91f66a3427c8155d8b1a62bb47160af6f3e414b
AdhaniMulianti <adhanimulianti34@gmail.com>
2023-06-26 08:12:52 +0700

    update Adhani.php
    mengubah about us.php, Adhani.php, DashboardUser.php, hani.jpg

---
+++ application/views/Adhani.php
@@ -0,0 +1,168 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+    <h1>
+            About Us
+            <br><small>Adhani Mulianti</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="<?php echo site_url('Home');?>"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li><a href="<?php echo site_url('Home');?>">Kelompok</a></li>
+            <li class="active">About Us</li>
+        </ol>
+    </section>
+    <br>
+
+  <title>Curriculum Vitae</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      background-color: #f2f2f2;
+      margin: 0;
+      padding: 0;
+    }
+
+    #container {
+      width: 800px;
+      margin: 20px auto;
+      background-color: #fff;
+      padding: 20px;
+    }
+
+    img {
+      display: block;
+      margin: 20px auto;
+      max-width: 200px;
+      border-radius: 50%;
+    }
+
+    .section {
+      margin-bottom: 20px;
+    }
+
+    .section h2 {
+      margin: 10px 0;
+    }
+
+    .section h3 {
+      margin: 5px 0;
+    }
+
+    .section p {
+      margin: 5px 0;
+    }
+
+    .section ul {
+      margin: 5px 0;
+      padding-left: 20px;
+    }
+
+    table {
+      width: 100%;
+      border-collapse: collapse;
+      margin-bottom: 10px;
+    }
+
+    th, td {
+      padding: 8px;
+      text-align: left;
+      border-bottom: 1px solid #ddd;
+    }
+
+    th {
+      background-color: #f2f2f2;
+    }
+  </style>
+
+  <div id="container">
+    <img src="http://localhost/RepositorySkripsi/Asset/img/hani.jpg" alt="Foto Profil">
+
+    <h1 style="text-align: center;"><b>Curriculum Vitae</b></h1>
+
+    <div class="section">
+      <h2><b>Data Pribadi</b></h2>
+      <table>
+        <tr>
+          <th>Nama</th>
+          <td>Adhani Mulianti</td>
+        </tr>
+        <tr>
+          <th>Alamat</th>
+          <td>Purwakarta</td>
+        </tr>
+        <tr>
+          <th>Email</th>
+          <td>adhanimulianti34@gmail.com</td>
+        </tr>
+        <tr>
+          <th>Telepon</th>
+          <td>081572185224</td>
+        </tr>
+      </table>
+    </div>
+
+    <div class="section">
+      <h2><b>Pendidikan</b></h2>
+      <table>
+        <tr>
+          <th>Nama Sekolah/Universitas</th>
+          <th>Tahun</th>
+          <th>Jurusan</th>
+        </tr>
+        <tr>
+          <td>SDN 2 Bunder</td>
+          <td>2007 - 2013</td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td>Mts PP Darussalam</td>
+          <td>2013 - 2016</td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td>SMAN 1 Purwakarta</td>
+          <td>2016 - 2019</td>
+          <td>IPA</td>
+        </tr>
+        <tr>
+          <td>Universitas Jenderal Achmad Yani</td>
+          <td>2020 - 2024</td>
+          <td>Informatika</td>
+        </tr>
+      </table>
+    </div>
+
+    <div class="section">
+      <h2><b>Pengalaman Kerja</b></h2>
+      <table>
+        <tr>
+          <th>Perusahaan</th>
+          <th>Jabatan</th>
+          <th>Tahun</th>
+        </tr>
+        <tr>
+          <td>Perusahaan Apple</td>
+          <td>Kepala Divisi IT</td>
+          <td>2024 - 2030</td>
+        </tr>
+      </table>
+    </div>
+    
+    <div class="section">
+      <h2><b>Keterampilan</b></h2>
+      <table>
+        <tr>
+          <th>Bahasa Pemrograman</th>
+          <th>Framework</th>
+          <th>Database</th>
+          <th>Desain Grafis</th>
+        </tr>
+        <tr>
+          <td>HTML, CSS, JavaScript</td>
+          <td>Laravel</td>
+          <td>MySQL</td>
+          <td>Figma</td>
+        </tr>
+      </table>
+    </div>
+  </div>
